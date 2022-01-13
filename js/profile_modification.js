1f558baecc540b070b69cad7abf065c0be4a7452
gureumwoon <vfrvfr3221@gmail.com>
2022-01-13 21:44:27 +0900

    주석추가 & input 유효성 검사 기능 추가

--- js/profile_modification.js
+++ js/profile_modification.js
@@ -3,6 +3,7 @@ const saveButton = document.querySelector('.nav > button');
 const userIdInput = document.querySelector('#user-id');
 const warningTextList = document.querySelectorAll('.warning-text');
 
+// 이름 2~10자 이내 유효성 검사
 const isUserNameInputValid = () => {
     const userName = userNameInput.value;
     if (userName.length < 2 || 10 < userName.length) {
@@ -11,6 +12,7 @@ const isUserNameInputValid = () => {
     return true;
 }
 
+// 영문, 숫자, ._ 만 가능한 유효성 검사
 const isUserIdInputValid = () => {
     const userId = userIdInput.value;
     const reg1 = /[A-Z]/g;
@@ -21,10 +23,12 @@ const isUserIdInputValid = () => {
     return isValid;
 }
 
+
+// 버튼 클릭 했을때 유효성 검사 (유효성 검사 실패시 저장버튼 안 눌림)
+
 saveButton.addEventListener('click', () => {
     if (!isUserNameInputValid()) {
         warningTextList[0].classList.remove('invisible');
-        console.log("2~10자 이내여야 합니다.");
         return;
     }
     warningTextList[0].classList.add('invisible');
@@ -35,6 +39,33 @@ saveButton.addEventListener('click', () => {
     warningTextList[1].classList.add('invisible');
     window.location.href = "my_profile.html";
 })
+
+// imput에 입력시 유효성 검사
+
+userNameInput.addEventListener('input', () => {
+    if (!isUserNameInputValid()) {
+        warningTextList[0].classList.remove('invisible');
+        if (userNameInput.value === "") {
+            warningTextList[0].classList.add('invisible');
+        }
+        return;
+    }
+    warningTextList[0].classList.add('invisible');
+    userIdInput.addEventListener('input', () => {
+        if (!isUserIdInputValid()) {
+            warningTextList[1].classList.remove('invisible');
+            if (userIdInput.value === "") {
+                warningTextList[1].classList.add('invisible');
+            }
+            return;
+        }
+        saveButton.classList.add('active');
+        warningTextList[1].classList.add('invisible');
+    })
+})
+
+
+// 버튼 활성화 (이름과 ID 입력시 버튼 활성화)
 
 const isButtonActive = () => {
     if (!isUserNameInputValid()) {
