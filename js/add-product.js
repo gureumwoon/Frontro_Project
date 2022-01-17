7672d61e47d60d981e20e8aba958100dc970794d
gureumwoon <vfrvfr3221@gmail.com>
2022-01-17 13:18:47 +0900

    버튼 활성화 추가 & 전체적인 코드 수정

--- js/add-product.js
+++ js/add-product.js
@@ -2,54 +2,73 @@ const productNameInput = document.querySelector('#product-name');
 const saveButton = document.querySelector('.nav > button');
 const productPriceInput = document.querySelector('#product-price');
 const warningTextList = document.querySelectorAll('.warning-text');
-
-const isProductNameInputValid = () => {
-    const productName = productNameInput.value;
-    if (productName.length < 2 || 15 < productName.length) {
-        return false;
-    }
-    return true;
-}
-
-const isPriceValid = () => {
-    const reg1 = /[0-9]/g;
-    const isValid = reg1.test(price);
-    return isValid; // 숫자일때 true출력
-}
+const productUrlInp = document.querySelector('#product-link')
 
 // 상품명 유효성 검사
 
 productNameInput.addEventListener('input', () => {
+    const isProductNameInputValid = () => {
+        const productName = productNameInput.value;
+        if (productName.length < 2 || 15 < productName.length) {
+            return false;
+        }
+        return true;
+    }
+
     if (!isProductNameInputValid()) {
         warningTextList[0].classList.remove('invisible');
         if (productNameInput.value === "") {
             warningTextList[0].classList.add('invisible');
         }
-        return;
+    } else {
+        warningTextList[0].classList.add('invisible');
+
+    }
+    uploadBtnCheck()
+})
+
+// 상품 가격 유효성 검사
+
+productPriceInput.addEventListener('input', () => {
+
+    const price = productPriceInput.value;
+
+    const isPriceValid = () => {
+        const reg1 = /[0-9]/g;
+        const isValid = reg1.test(price);
+        console.log(isValid); // 숫자일때 true출력
+        return isValid;
     }
 
-    // 상품 가격 유효성 검사
-
-    warningTextList[0].classList.add('invisible');
-    productPriceInput.addEventListener('input', () => {
-        const price = productPriceInput.value;
-        if (isPriceValid()) {
-            const productPrice = Number(price.split(',').join(""));
-            const productPriceLocale = productPrice.toLocaleString();
-            productPriceInput.value = productPriceLocale;
-            warningTextList[1].classList.add('invisible');
-        }
-        else if (productPriceInput.value === '') {
-            warningTextList[1].classList.add('invisible')
-        } else {
-            stringOut();
-        }
-        function stringOut() {
-            productPriceInput.value = '';
-            warningTextList[1].classList.remove('invisible');
-        }
-        saveButton.classList.add('active');
-        warningTextList[0].classList.add('invisible');
-    })
+    if (isPriceValid()) {
+        const productPrice = Number(price.split(',').join(""));
+        const productPriceLocale = productPrice.toLocaleString();
+        productPriceInput.value = productPriceLocale;
+        warningTextList[1].classList.add('invisible');
+    } else if (productPriceInput.value === '') {
+        warningTextList[1].classList.add('invisible')
+    } else {
+        warningTextList[1].classList.remove('invisible');
+    }
+    uploadBtnCheck()
 })
+
+productUrlInp.addEventListener('input', () => {
+    uploadBtnCheck()
+})
+
+// 버튼 활성화
+
+function uploadBtnCheck() {
+    if (productNameInput.value && productPriceInput.value && productUrlInp.value) {
+        saveButton.disabled = false;
+        saveButton.classList.add('active')
+    } else {
+        saveButton.disabled = true;
+        saveButton.classList.remove('active')
+    }
+}
+
+
+
 
