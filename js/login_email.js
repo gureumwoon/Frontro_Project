bb48613d18accc4a80d076c1f9eececece1e80e5
Moonhokyun <soulty321@gmail.com>
2022-01-21 10:49:03 +0900

    🐛 아까 충돌 났던 부분 수정

--- js/login_email.js
+++ js/login_email.js
@@ -4,36 +4,7 @@ function getInput() {
 }
 // 비동기로 동하는것을 명시해줘야 await을 할 수 있다!
 async function login() {
-<<<<<<< HEAD
-    const email = document.querySelector("#input-box-id").value;
-    const pw = document.querySelector("#input-box-pw").value;
-    const url = "http://146.56.183.55:5050";
-    const loginData = {
-        user: {
-            email: email,
-            password: pw,
-        },
-    };
 
-    const res = await fetch(url + "/user/login", {
-        //메소드 구분
-        method: "POST",
-        //헤더
-        headers: {
-            "Content-type": "application/json",
-        },
-        //이건 오브잭트를 문자열로 바꿔주는 부분
-        body: JSON.stringify(loginData),
-    });
-    const json = await res.json(); //외않됌? 포인트 res.json()도 비동기. await을 해줘야한다.
-    localStorage.setItem("Token", json.user.token);
-    localStorage.setItem("accountName", json.user.accountname);
-    localStorage.setItem("user-profile", json.user.image);
-    location.href = "./index.html";
-}
-const $loginBtn = document.querySelector("#login_btn");
-$loginBtn.addEventListener("click", login);
-=======
   const email = document.querySelector("#input-box-id").value
   const pw = document.querySelector("#input-box-pw").value
   const url = "http://146.56.183.55:5050"
@@ -64,4 +35,3 @@ $loginBtn.addEventListener("click", login);
 }
 const $loginBtn = document.querySelector('#login_btn')
 $loginBtn.addEventListener("click", login)
->>>>>>> af208561b64894c2a8173cf5586fba05963fbd6f
