ba1f3b02a2e84020db7fd37d59e0529e2d26db06
gureumwoon <vfrvfr3221@gmail.com>
2022-01-23 17:25:08 +0900

    게시물 수정 코드 추가

--- js/upload.js
+++ js/upload.js
@@ -1,6 +1,9 @@
+const profileImg = document.querySelector('.profile-pic');
 const textarea = document.querySelector('.upload-txt');
 const prevImg = document.querySelector('.prev-img');
-const backBtn = document.querySelector('.back-btn')
+const backBtn = document.querySelector('.back-btn');
+const txtContent = document.querySelector('.post-content> textarea');
+const token = localStorage.getItem("Token")
 let imgIndex = 0;
 const formData = new FormData();
 
@@ -81,7 +84,6 @@ async function imageUpload(files, index) {
 }
 async function createPost(e) {
     const url = "http://146.56.183.55:5050"
-    const token = localStorage.getItem("Token")
     //입력한 텍스트 불러와야함
     const contentText = $content.value
     //여기는 나중에 이미지 주소가 추가될 예정
@@ -109,17 +111,107 @@ async function createPost(e) {
             })
         })
         const json = await res.json()
-        console.log(json)
-        // window.location.href = "index.html"
+        console.log(json);
+        location.href = "my_profile.html"
     } else {
         alert("아 이미지 갯수가 너무 많소")
     }
 
 }
 //여기까지 이미지 여러개 업로드하기.
-$submitBtn.addEventListener('click', (e) => {
-    createPost()
+
+async function getPostData() {
+    const queryString = window.location.href.split('?')[1]
+    const searchParams = new URLSearchParams(queryString)
+    console.log("searchparams: ", searchParams)
+    const postId = searchParams.get('id');
+    const res = await fetch(`http://146.56.183.55:5050/post/${postId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+    });
+    const data = await res.json();
+    console.log(data);
+    profileImg.src = data.post.author.image
+    txtContent.value = data.post.content;
+    dataImg = data.post.image
+    for (const contentImg of dataImg) {
+        prevImg.innerHTML += `
+     <li>
+        <img src=${contentImg}  alt="업로드 할 이미지1">
+        <button class="img-cancel-btn">
+            <img src="src/png/x.png" alt="사진 업로드 취소 버튼">
+        </button>
+     </li>
+     `
+        $submitBtn.classList.add('active')
+    }
+    deletePrevImg(dataImg);
+}
+
+if (queryString) {
+    getPostData();
+}
+
+async function putData() {
+    const queryString = window.location.href.split('?')[1]
+    const searchParams = new URLSearchParams(queryString)
+    console.log("searchparams: ", searchParams)
+    const postId = searchParams.get('id');
+    const imageUrls = []
+    const content = textarea.value;
+
+    const res = await fetch(`http://146.56.183.55:5050/post/${postId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+            post: {
+                content: content,
+                image: imageUrls + '',
+            },
+        }),
+    });
+    const data = await res.json();
+    console.log(data);
+    if (data) {
+        alert('업로드 성공');
+        location.href = `/pages/post.html?${data.post.id}`;
+    } else {
+        alert('업로드 실패');
+    }
+}
+
+txtContent.addEventListener('input', () => {
+    uploadBtnCheck()
 })
+
+function uploadBtnCheck() {
+    if (txtContent.value && ($image.value || dataImg.length)) {
+        $submitBtn.disabled = false;
+        $submitBtn.classList.add('active')
+    } else {
+        $submitBtn.disabled = true;
+        $submitBtn.classList.remove('active')
+    }
+}
+
+$submitBtn.addEventListener('click', (e) => {
+    const queryString = window.location.href.split('?')[1]
+    const searchParams = new URLSearchParams(queryString)
+    console.log("searchparams: ", searchParams)
+    const postId = searchParams.get('id');
+    if (postId) {
+        putData()
+    } else {
+        createPost()
+    }
+});
+
 
 
 
